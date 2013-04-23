9beddcbadb48038bdddca7f5bddba7548c7a7547
Mark Norman Francis <mark.norman.francis@digital.cabinet-office.gov.uk>
2013-04-23 12:05:01 +0100

    Monocle.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install Monocle to /Applications
+#
+# Examples
+#
+#   include monocle
+
+class monocle {
+  package { 'Monocle':
+    provider => 'compressed_app',
+    source   => 'http://wafflesoftware.net/monocle/download/Monocle_1.6.2_61.zip'
+  }
 }
--- spec/classes/template_spec.rb
+++ spec/classes/template_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
-# Rename this file to classname_spec.rb
-# Check other boxen modules for examples
-# or read http://rspec-puppet.com/tutorial/
-describe 'template' do
+
+describe 'monocle' do
   it do
-    should contain_anchor('Hello_World')
+    should contain_package('Monocle').with({
+      :provider => 'compressed_app',
+      :source   => 'http://wafflesoftware.net/monocle/download/Monocle_1.6.2_61.zip',
+    })
   end
 end
